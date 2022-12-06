907f150ef08a9975b4f402e0bf46f78631dc2dd1
Ignacio García Oliver <igarciaoliver@gmail.com>
2022-12-06 13:38:03 -0300

    weird

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,16 @@ import {
     TonemapPlugin,
     SSRPlugin,
     SSAOPlugin,
-    DiamondPlugin,
+    // DiamondPlugin,
     FrameFadePlugin,
     GLTFAnimationPlugin,
     GroundPlugin,
-    BloomPlugin,
-    TemporalAAPlugin,
-    AnisotropyPlugin,
-    GammaCorrectionPlugin,
+    // BloomPlugin,
+    // TemporalAAPlugin,
+    // AnisotropyPlugin,
+    // GammaCorrectionPlugin,
+
+    PickingPlugin,
 
     addBasePlugins,
     ITexture, TweakpaneUiPlugin, AssetManagerBasicPopupPlugin, CanvasSnipperPlugin,
@@ -23,8 +25,13 @@ import {
 
     // Color, // Import THREE.js internals
     // Texture, // Import THREE.js internals
+    Vector3
 } from "webgi";
-import "./styles.css";
+
+
+
+
+import "./styles.scss";
 
 async function setupViewer(){
 
@@ -40,8 +47,8 @@ async function setupViewer(){
     await viewer.addPlugin(AssetManagerBasicPopupPlugin)
 
     // Add plugins individually.
-    // await viewer.addPlugin(GBufferPlugin)
-    // await viewer.addPlugin(new ProgressivePlugin(32))
+    await viewer.addPlugin(GBufferPlugin)
+    await viewer.addPlugin(new ProgressivePlugin(32))
     // await viewer.addPlugin(new TonemapPlugin(!viewer.useRgbm))
     // await viewer.addPlugin(GammaCorrectionPlugin)
     // await viewer.addPlugin(SSRPlugin)
@@ -54,29 +61,51 @@ async function setupViewer(){
     // await viewer.addPlugin(TemporalAAPlugin)
     // await viewer.addPlugin(AnisotropyPlugin)
 
+    
     // or use this to add all main ones at once.
-    await addBasePlugins(viewer)
-
+    // await addBasePlugins(viewer)
+    
     // Add more plugins not available in base, like CanvasSnipperPlugin which has helpers to download an image of the canvas.
-    await viewer.addPlugin(CanvasSnipperPlugin)
+    // await viewer.addPlugin(CanvasSnipperPlugin)
+    
+    /**
+     * Start of my code....
+     * 
+     */
 
+    viewer.scene.activeCamera.position.set(2, 3, -5);
+    viewer.scene.activeCamera.positionUpdated(); // this must be called to notify the controller on value update
+    viewer.scene.activeCamera.target.set(0, 2, 0);
+    viewer.scene.activeCamera.targetUpdated(); 
+
+    //
+    const picking = viewer.addPlugin(PickingPlugin);
     // This must be called once after all plugins are added.
     viewer.renderer.refreshPipeline()
-
-    await manager.addFromPath("./assets/classic-watch.glb")
+    const loadingOptions = {
+        autoScale: false, // Scales the object before adding to the scene.
+        autoScaleRadius: 2, // Scales the object bounding box to 2 World Units, if autoScale is true
+        pseudoCenter: true,
+     };
+    // await manager.addFromPath("./assets/environment.glb", loadingOptions)
+    // await manager.addFromPath("./assets/domo.glb", loadingOptions)
+    await manager.addFromPath("./assets/combined.zip", loadingOptions)
 
     // Load an environment map if not set in the glb file
-    // await viewer.scene.setEnvironment(
-    //     await manager.importer!.importSinglePath<ITexture>(
-    //         "./assets/environment.hdr"
-    //     )
-    // );
+    await viewer.scene.setEnvironment(
+        await manager.importer!.importSinglePath<ITexture>(
+            "./assets/hdr/rotated_2k.hdr"
+        )
+    );
 
-    // Add some UI for tweak and testing.
-    const uiPlugin = await viewer.addPlugin(TweakpaneUiPlugin)
-    // Add plugins to the UI to see their settings.
-    uiPlugin.setupPlugins<IViewerPlugin>(TonemapPlugin, CanvasSnipperPlugin)
 
+    const centerCamerabtn = document.getElementById('centerCamera');
+    centerCamerabtn?.addEventListener('click', (evt) =>{
+        viewer.scene.activeCamera.position.set(1, 1, 1);
+        viewer.scene.activeCamera.positionUpdated(); // this must be called to notify the controller on value update
+        viewer.scene.activeCamera.target.set(0, 0, 0);
+        viewer.scene.activeCamera.targetUpdated(); 
+    })
 }
 
 setupViewer()
